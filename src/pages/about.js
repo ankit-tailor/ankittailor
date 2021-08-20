02184a339f0b7ee4354c2ce2d6e7929d2751beb4
ankit-tailor <tailorankit3345@gmail.com>
2021-08-20 13:24:31 +0530

    page: about

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout/Layout"
+
+function about() {
+  return (
+    <Layout>
+      <h1>About page</h1>
+    </Layout>
+  )
+}
+
+export default about
