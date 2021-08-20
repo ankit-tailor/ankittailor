3e49cd3834fa25eadc0f80b7eb2d7f73907ae822
ankit-tailor <tailorankit3345@gmail.com>
2021-08-20 13:25:45 +0530

    page: blog listing

---
+++ src/pages/blogs/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../../components/layout/Layout"
+
+function Blogs() {
+  return (
+    <Layout>
+      <h1>Blogs</h1>
+    </Layout>
+  )
+}
+
+export default Blogs
