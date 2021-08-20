0e9fbafe37baad16e58aff069abf0e13ba159643
ankit-tailor <tailorankit3345@gmail.com>
2021-08-20 13:27:11 +0530

    theme provider

---
+++ src/providers/ThemeProvider.js
@@ -0,0 +1,32 @@
+import React, { createContext, useEffect } from "react"
+import useDarkMode from "../hooks/useDarkMode"
+
+export const ThemeContext = createContext("light")
+const isBrowser = typeof window !== "undefined"
+
+export default ({ children }) => {
+  const [theme, toggleTheme] = useDarkMode()
+
+  useEffect(() => {
+    if (
+      (isBrowser && window.localStorage.theme === "dark") ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.documentElement.classList.add("dark")
+    } else {
+      document.documentElement.classList.remove("dark")
+    }
+  }, [toggleTheme])
+
+  return (
+    <ThemeContext.Provider
+      value={{
+        theme,
+        toggleTheme,
+      }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  )
+}
