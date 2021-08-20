e7d0befb6b7c91dd5355f98a531f02fb45ce6e64
ankit-tailor <tailorankit3345@gmail.com>
2021-08-20 13:24:51 +0530

    configure layout

---
+++ src/components/layout/Layout.js
@@ -0,0 +1,18 @@
+import React from "react"
+import ThemeProvider from "../../providers/ThemeProvider"
+import Footer from "../footer/Footer"
+import Navbar from "../header/Navbar"
+
+const Layout = ({ children }) => {
+  return (
+    <ThemeProvider>
+      <div>
+        <Navbar />
+        {children}
+        <Footer />
+      </div>
+    </ThemeProvider>
+  )
+}
+
+export default Layout
