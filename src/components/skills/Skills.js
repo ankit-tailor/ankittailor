f5aa237559aa60829a84dde837fd973c747fa5cb
ankit-tailor <tailorankit3345@gmail.com>
2021-08-20 13:23:52 +0530

    section: skills

---
+++ src/components/skills/Skills.js
@@ -0,0 +1,50 @@
+import React from "react"
+import {
+  faCss3Alt,
+  faHtml5,
+  faJs,
+  faNodeJs,
+  faNpm,
+  faReact,
+} from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+const Skills = () => {
+  return (
+    <div className="text-dark dark:text-white bg-white dark:bg-dark">
+      <div className="sm:container sm:mx-auto">
+        <div className="center py-10 px-10 sm:px-20 sm:py-20">
+          <h1 className="text-center font-bold text-2xl">Skills</h1>
+          <div className="text-center md:justify-center md:content-center my-10">
+            <FontAwesomeIcon
+              className="text-6xl sm:text-6xl md:text-8xl mx-8 cursor-pointer sm:mx-10 my-8 font-medium"
+              icon={faHtml5}
+            />
+            <FontAwesomeIcon
+              className="text-6xl sm:text-6xl md:text-8xl mx-8 cursor-pointer sm:mx-10 my-8 font-medium"
+              icon={faCss3Alt}
+            />
+            <FontAwesomeIcon
+              className="text-6xl sm:text-6xl md:text-8xl mx-8 cursor-pointer sm:mx-10 my-8 font-medium"
+              icon={faJs}
+            />
+            <FontAwesomeIcon
+              className="text-6xl sm:text-6xl md:text-8xl mx-8 cursor-pointer sm:mx-10 my-8 font-medium"
+              icon={faReact}
+            />
+            <FontAwesomeIcon
+              className="text-6xl sm:text-6xl md:text-8xl mx-8 cursor-pointer sm:mx-10 my-8 font-medium"
+              icon={faNpm}
+            />
+            <FontAwesomeIcon
+              className="text-6xl sm:text-6xl md:text-8xl mx-8 cursor-pointer sm:mx-10 my-8 font-medium"
+              icon={faNodeJs}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Skills
