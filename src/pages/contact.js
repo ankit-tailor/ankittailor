5e56cbc36dd0eff0e169ec86bd12d495d6f89626
ankit-tailor <tailorankit3345@gmail.com>
2021-08-20 13:25:05 +0530

    page: contact

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout/Layout"
+
+function contact() {
+  return (
+    <Layout>
+      <h1>Contacts page</h1>
+    </Layout>
+  )
+}
+
+export default contact
