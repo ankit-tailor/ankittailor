e14c4c4db800c420fed0b3d2b590ec697c1d47b2
ankittailor10 <ankit.tailor@isimplexity.com>
2021-08-26 16:43:21 +0530

    add dummy project cards

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from "react"
+import Blog from "../components/blogs/Blog"
 import Intro from "../components/intro/Intro"
 import Layout from "../components/layout/Layout"
+import Project from "../components/projects/Project"
 import Skills from "../components/skills/Skills"
 
 const Home = () => {
@@ -8,6 +10,22 @@ const Home = () => {
     <Layout>
       <Intro />
       <Skills />
+      <div className="text-dark dark:text-white bg-white dark:bg-dark">
+        <div className="sm:container sm:mx-auto">
+          <div className="p-6">
+            <h1 className="text-center font-bold text-2xl">Recent Projects</h1>
+            <div className="gap-5 my-6 grid place-items-center lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2">
+              <Project />
+              <Project />
+              <Project />
+              <Project />
+            </div>
+            <button className="block transform-scale shadow-2xl w-6/12 lg:w-4/12 bg-indigo text-white py-2 mx-auto my-10 rounded px-4">
+              See More
+            </button>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
