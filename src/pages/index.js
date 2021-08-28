b96317cb7050eda1fead5f5b0997f4725ec68107
ankittailor10 <ankit.tailor@isimplexity.com>
2021-08-28 13:01:50 +0530

    add blog listing

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,6 +26,22 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <div className="text-dark dark:text-white bg-white dark:bg-dark">
+        <div className="sm:container sm:mx-auto">
+          <div className="p-2">
+            <h1 className="text-center font-bold text-2xl">Recent Blogs</h1>
+            <div className="gap-3 grid place-items-center lg:grid-cols-2 md:grid-cols-1">
+              <Blog />
+              <Blog />
+              <Blog />
+              <Blog />
+            </div>
+            <button className="block transform-scale shadow-2xl w-6/12 lg:w-4/12 bg-indigo text-white py-2 mx-auto my-10 rounded px-4">
+              See More
+            </button>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
