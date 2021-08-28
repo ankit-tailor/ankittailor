07c54ebdb27a7e1e400d4b85c64408c9e46bf40d
ankittailor10 <ankit.tailor@isimplexity.com>
2021-08-28 13:02:54 +0530

    blog component

---
+++ src/components/blogs/Blog.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Typography from '@material-ui/core/Typography';
+
+const Blog = () => {
+    return (
+        <div className="p-6 cursor-pointer bg-white text-dark dark:bg-dark dark:text-white">
+            <div className="flex">
+                <img className="w-3/12 mx-4" src="https://images.pexels.com/photos/4533077/pexels-photo-4533077.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="" />
+                <div className="flex flex-col justify-evenly">
+                    <div>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            Lizard
+                        </Typography>
+                        <Typography variant="body2" component="p">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000 species...
+                        </Typography>
+                    </div>
+                    <Typography variant="body2" className="text-active" component="p">
+                        28th Sep, 2021
+                    </Typography>
+                </div>
+            </div>
+            <hr className="w-full mx-auto my-3" />
+        </div>
+    )
+}
+
+export default Blog
