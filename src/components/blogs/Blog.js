cca1eff57fa3a2b672597da1111feb8bd40aad40
ankit-tailor <tailorankit3345@gmail.com>
2021-08-28 17:14:44 +0530

    add shadow, border radius

--- src/components/blogs/Blog.js
+++ src/components/blogs/Blog.js
@@ -3,9 +3,9 @@ import Typography from '@material-ui/core/Typography';
 
 const Blog = () => {
     return (
-        <div className="p-6 cursor-pointer bg-white text-dark dark:bg-dark dark:text-white">
+        <div className="p-6 rounded-lg shadow-2xl my-2 cursor-pointer bg-white text-dark dark:bg-dark dark:text-white">
             <div className="flex">
-                <img className="w-3/12 mx-4" src="https://images.pexels.com/photos/4533077/pexels-photo-4533077.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="" />
+                <img className="w-3/12 shadow-2xl mx-4" src="https://images.pexels.com/photos/4533077/pexels-photo-4533077.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="" />
                 <div className="flex flex-col justify-evenly">
                     <div>
                         <Typography gutterBottom variant="h5" component="h2">
@@ -20,7 +20,7 @@ const Blog = () => {
                     </Typography>
                 </div>
             </div>
-            <hr className="w-full mx-auto my-3" />
+            <hr className="w-full mx-auto my-2" />
         </div>
     )
 }
