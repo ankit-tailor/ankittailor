5a86498818f4a1e61864109c1a0ae3583679223b
ankit-tailor <tailorankit3345@gmail.com>
2021-08-29 14:58:27 +0530

    feat: projects component

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,10 +20,15 @@ const Home = ({ data }) => {
           <div className="p-6">
             <h1 className="text-center font-bold text-2xl">Recent Projects</h1>
             <div className="gap-5 my-6 grid place-items-center lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2">
-              <Project />
-              <Project />
-              <Project />
-              <Project />
+              {allProjects.map(({ frontmatter: { name, techstack, github, liveProject }, id }) => (
+                <Project
+                  key={id}
+                  name={name}
+                  liveProject={liveProject}
+                  techstack={techstack}
+                  github={github}
+                />
+              ))}
             </div>
             <button className="block transform-scale shadow-2xl w-6/12 lg:w-4/12 bg-indigo text-white py-2 mx-auto my-10 rounded px-4">
               See More
