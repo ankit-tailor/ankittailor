475f4401b5326e8e4b6be566fd2c87fe646009cd
ankit-tailor <tailorankit3345@gmail.com>
2021-08-29 18:36:57 +0530

    feat:  optimized gatsby img

--- src/components/blogs/Blog.js
+++ src/components/blogs/Blog.js
@@ -1,26 +1,29 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography';
+import Img from "gatsby-image"
 
-const Blog = () => {
+const Blog = ({ title, description, fluid, date, link }) => {
     return (
-        <div className="p-6 rounded-lg shadow-2xl my-2 cursor-pointer bg-white text-dark dark:bg-dark dark:text-white">
-            <div className="flex">
-                <img className="w-3/12 shadow-2xl mx-4" src="https://images.pexels.com/photos/4533077/pexels-photo-4533077.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="" />
-                <div className="flex flex-col justify-evenly">
-                    <div>
-                        <Typography gutterBottom variant="h5" component="h2">
-                            Lizard
-                        </Typography>
-                        <Typography variant="body2" component="p">
-                            Lizards are a widespread group of squamate reptiles, with over 6,000 species...
+        <div className="p-6 rounded-lg shadow-2xl my-2 cursor-pointer bg-white text-dark dark:bg-dark dark:text-white w-full">
+            <a href={link} target="_blank" rel="noopener">
+                <div className="flex">
+                    <Img className="w-4/12 max-h-30 transform-scale shadow-2xl mx-2" fluid={fluid} alt={title} />
+                    <div className="flex flex-col justify-evenly">
+                        <div>
+                            <Typography gutterBottom variant="h5" component="h2">
+                                {title.length > 30 ? `${title.substring(0, 30)}...` : `${title}`}
+                            </Typography>
+                            <Typography variant="body2" component="p">
+                                {`${description?.substring(0, 150)}...`}
+                            </Typography>
+                        </div>
+                        <Typography variant="body2" className="text-active" component="p">
+                            {date}
                         </Typography>
                     </div>
-                    <Typography variant="body2" className="text-active" component="p">
-                        28th Sep, 2021
-                    </Typography>
                 </div>
-            </div>
-            <hr className="w-full mx-auto my-2" />
+                <hr className="w-full mx-auto my-2" />
+            </a>
         </div>
     )
 }
