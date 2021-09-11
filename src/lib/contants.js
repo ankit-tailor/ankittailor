fb2877b60f4c7731f186c95cd8f12bce26139e92
ankit-tailor <tailorankit3345@gmail.com>
2021-09-12 04:05:56 +0530

    update resume link

--- src/lib/contants.js
+++ src/lib/contants.js
@@ -12,6 +12,6 @@ const SOCIAL_LINKS = {
   EMAIL: "tailorankit3345@gmail.com"
 }
 
-const RESUME = "https://drive.google.com/file/d/1asKAMLexNMWkBiraY0TeBr6T1mtjo_tl/view?usp=sharing"
+const RESUME = "https://drive.google.com/file/d/1a1jASlWFRdDV62qulxk6uaKIEoQDZR4G/view?usp=sharing"
 
 export { PROJECT_REPO, PROJECT_LINK, SOCIAL_LINKS, RESUME }
