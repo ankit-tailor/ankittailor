c27000616f311bf1b80170418407dac38942917a
ankit-tailor <tailorankit3345@gmail.com>
2021-09-12 02:35:35 +0530

    feat: ProjectList component

---
+++ src/components/project/ProjectList.js
@@ -0,0 +1,50 @@
+import React from "react"
+import Card from "@material-ui/core/Card"
+import CardActions from "@material-ui/core/CardActions"
+import CardContent from "@material-ui/core/CardContent"
+import Typography from "@material-ui/core/Typography"
+import Img from "gatsby-image"
+
+function ProjectList({ name, github, liveProject, techstack, fluid }) {
+    return (
+        <Card className="py-8 shadow-2xl bg-white cursor-pointer dark:bg-dark text-dark dark:text-white w-11/12 lg:w-10/12 mx-auto my-10">
+            <Img
+                fluid={fluid}
+                className="h-80 transform-scale ease-in"
+                atl="banner image"
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                    {name}
+                </Typography>
+                <Typography variant="body2" component="p">
+                    {techstack}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <button
+                    size="small"
+                    className="transform-scale"
+                    style={{ color: "rgba(0, 195, 255)", fontSize: "0.9rem" }}
+                >
+                    <a href={liveProject} target="_blank" rel="noreferrer">
+                        {" "}
+                        Live Project{" "}
+                    </a>
+                </button>
+                <button
+                    size="small"
+                    className="transform-scale"
+                    style={{ color: "rgba(0, 195, 255)", fontSize: "0.9rem" }}
+                >
+                    <a href={github} target="_blank" rel="noreferrer">
+                        {" "}
+                        Source Code
+                    </a>
+                </button>
+            </CardActions>
+        </Card>
+    )
+}
+
+export default ProjectList
