4c1a0f888185a7d57ff8d2f76f9c48b09b0ae765
ankit-tailor <tailorankit3345@gmail.com>
2021-09-12 02:42:23 +0530

    feat: contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,25 @@
 import React from "react"
 import Layout from "../components/layout/Layout"
+import SEO from "../components/SEO/seo"
 
 function contact() {
   return (
     <Layout>
-      <h1>Contacts page</h1>
+      <SEO siteTitle="Contact" />
+      <div className="dark:bg-dark bg-white text-dark dark:text-white">
+        <div className="lg:container py-4 lg:mx-auto">
+          <div className="py-2 lg:px-5 lg:px-20 sm:py-2">
+            <h1 className="text-4xl sm:text-3xl mx-8 my-3 font-bold">
+              <span className="text-indigo underline">
+                Contact
+                <span role="img" aria-label="wave">
+                  🚀
+                </span>
+              </span>
+            </h1>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
