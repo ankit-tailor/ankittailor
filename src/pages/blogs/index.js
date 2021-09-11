47b646e73d9c5d4572832cb10992ada6c39972df
ankit-tailor <tailorankit3345@gmail.com>
2021-09-12 02:36:00 +0530

    feat: graphql query for blogs, BlogList component for blog

--- src/pages/blogs/index.js
+++ src/pages/blogs/index.js
@@ -2,19 +2,24 @@ import React from "react"
 import Layout from "../../components/layout/Layout"
 import { graphql, Link } from "gatsby"
 import SEO from "../../components/SEO/seo"
-import Blog from "../../components/blog/Blog"
+import BlogList from "../../components/blog/BlogList"
 
 function Blogs({ data }) {
   const allBlogs = data.blogs.edges
 
   return (
     <Layout>
+      <SEO siteTitle="Blogs" />
       <div className="dark:bg-dark bg-white text-dark dark:text-white">
-        <div className="sm:container py-4 sm:mx-auto">
-          <div className="py-2 px-5 sm:px-20 sm:py-2">
-            <SEO siteTitle="Blogs" />
-            <h1 className="text-2xl sm:text-3xl my-3 font-bold text-indigo">
-              Blogs
+        <div className="lg:container py-4 lg:mx-auto">
+          <div className="py-2 lg:px-5 lg:px-20 sm:py-2">
+            <h1 className="text-4xl sm:text-3xl mx-8 my-3 font-bold">
+              <span className="text-indigo underline">
+                Blogs
+                <span role="img" aria-label="wave">
+                  📚
+                </span>
+              </span>
             </h1>
             {allBlogs.map(
               ({
@@ -25,7 +30,7 @@ function Blogs({ data }) {
                 },
                 id,
               }) => (
-                <Blog
+                <BlogList
                   key={id}
                   title={title}
                   description={description}
