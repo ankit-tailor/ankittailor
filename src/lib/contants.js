ed6bd76073384f6cb38e71abaf432aeb3c3a64ea
ankit-tailor <tailorankit3345@gmail.com>
2021-09-11 15:47:30 +0530

    constants for social link, project repo and link

---
+++ src/lib/contants.js
@@ -0,0 +1,18 @@
+const PROJECT_REPO = "https://github.com/ankit-tailor/ankittailor"
+
+const PROJECT_LINK = "ankittailor.tech"
+
+const SOCIAL_LINKS = {
+    STACKOVERFLOW: "https://stackoverflow.com/users/11812542/ankit-tailor",
+    LINKEDIN: "https://linkedin.com/in/ankit-tailor",
+    HASHNODE: "https://blog.ankittailor.tech",
+    TWITTER: "https://twitter.com/ankit__tailor",
+    GITHUB: "https://github.com/ankit-tailor",
+    INSTAGRAM: "https://instagram.com/_akkiiii._"
+}
+
+export {
+    PROJECT_REPO,
+    PROJECT_LINK,
+    SOCIAL_LINKS
+}
