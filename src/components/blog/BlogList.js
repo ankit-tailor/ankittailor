43d5cd6217b532082e225fe25d808a6767eb2314
ankit-tailor <tailorankit3345@gmail.com>
2021-09-12 02:34:43 +0530

    feat: BlogList component

--- src/components/blog/BlogList.js
+++ src/components/blog/BlogList.js
@@ -2,39 +2,36 @@ import React from "react"
 import Typography from "@material-ui/core/Typography"
 import Img from "gatsby-image"
 
-const Blog = ({ title, description, fluid, date, link }) => {
-  return (
-    <div className="p-6 max-h-30 rounded-lg shadow-2xl my-2 cursor-pointer bg-white text-dark dark:bg-dark dark:text-white w-full">
-      <a href={link} target="_blank" rel="noreferrer">
-        <div className="flex">
-          <Img
-            className="w-6/12 sm:inline hidden transform-scale shadow-2xl mx-2"
-            fluid={fluid}
-            alt={title}
-          />
-          <div className="flex flex-col justify-evenly">
-            <div>
-              <Typography gutterBottom variant="h5" component="h2">
-                {title.length > 30
-                  ? `${title.substring(0, 30)}...`
-                  : `${title}`}
-              </Typography>
-              <Typography variant="body2" className="text-active" component="p">
-                {`${description?.substring(0, 150)}...`}
-              </Typography>
-            </div>
-            <Typography variant="body2" className="text-active" component="p">
-              <span role="img" aria-label="wave">
-                ⏱
-              </span>
-              {date}
-            </Typography>
-          </div>
+const BlogList = ({ title, description, fluid, date, link }) => {
+    return (
+        <div className="p-6 my-2 cursor-pointer bg-white text-dark dark:bg-dark dark:text-white hover:text-indigo w-full">
+            <a href={link} target="_blank" rel="noreferrer">
+                <div className="flex flex-col-reverse md:flex-row">
+                    <div className="flex hover:text-indigo flex-col justify-evenly">
+                        <div>
+                            <h1 className="font-extrabold my-4 text-4xl">
+                                {title}
+                            </h1>
+                            <Typography variant="body2" className="my-4 text-active" component="p">
+                                {description}
+                            </Typography>
+                        </div>
+                        <Typography variant="body2" className="my-4 text-active" component="p">
+                            <span role="img" aria-label="wave">
+                                ⏱
+                            </span>
+                            {date}
+                        </Typography>
+                    </div>
+                    <Img
+                        className="w-full my-4 lg:mx-4 h-80 lg:h-64 block rounded-lg transform-scale shadow-2xl mx-auto"
+                        fluid={fluid}
+                        alt={title}
+                    />
+                </div>
+            </a>
         </div>
-        <hr className="w-full mx-auto my-2" />
-      </a>
-    </div>
-  )
+    )
 }
 
-export default Blog
+export default BlogList
