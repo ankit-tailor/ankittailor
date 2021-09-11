d5c759152ea448398b3b6e3def1871d6bf32b2f4
ankit-tailor <tailorankit3345@gmail.com>
2021-09-11 16:31:49 +0530

    feat: add SEO

---
+++ src/components/SEO/seo.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { useStaticQuery, graphql } from 'gatsby';
+
+const SEO = ({ siteTitle = "" }) => {
+    const data = useStaticQuery(graphql`
+    {
+      site {
+        siteMetadata {
+          title
+          description
+          siteUrl,
+          url,
+          image,
+          twitter,
+          author
+        }
+      }
+    }
+  `);
+
+    const defaults = data.site.siteMetadata;
+
+    const title = `${defaults.title} • ${siteTitle}`;
+    const description = defaults.description;
+    const url = defaults.siteUrl
+    const image = ""
+
+    return (
+        <Helmet>
+            <title>{title}</title>
+            <link rel="canonical" href={url} />
+            <meta name="description" content={description} />
+            {image && <meta name="image" content={image} />}
+
+            <meta property="og:url" content={url} />
+            <meta property="og:type" content="article" />
+            <meta property="og:title" content={title} />
+            <meta property="og:description" content={description} />
+            {image && <meta property="og:image" content={image} />}
+
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:creator" content={defaults.twitter} />
+            <meta name="twitter:title" content={title} />
+            <meta name="twitter:description" content={description} />
+            {image && <meta name="twitter:image" content={image} />}
+        </Helmet>
+    );
+};
+
+export default SEO;
