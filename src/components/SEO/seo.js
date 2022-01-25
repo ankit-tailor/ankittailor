c23fd12959c9b849fc488522442e142884d0905e
ankit-tailor <tailorankit3345@gmail.com>
2022-01-25 23:34:15 +0530

    fix: image url

--- src/components/SEO/seo.js
+++ src/components/SEO/seo.js
@@ -25,25 +25,27 @@ const SEO = ({ siteTitle = "" }) => {
   const description = defaults.description
   const url = defaults.siteUrl
   const image = defaults.image
+  const defaultUrl = "https://ankittailor.tech"
+  console.log(defaultUrl + image)
 
   return (
     <Helmet>
       <title>{title}</title>
       <link rel="canonical" href={url} />
       <meta name="description" content={description} />
-      {image && <meta name="image" content={image} />}
+      {image && <meta name="image" content={`${defaultUrl}${image}`} />}
 
       <meta property="og:url" content={url} />
       <meta property="og:type" content="article" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      {image && <meta property="og:image" content={image} />}
+      {image && <meta property="og:image" content={`${defaultUrl}${image}`} />}
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={defaults.twitter} />
       <meta name="twitter:title" content={defaults.title} />
       <meta name="twitter:description" content={defaults.twitterDescription} />
-      {image && <meta name="twitter:image" content={image} />}
+      {image && <meta name="twitter:image" content={`${defaultUrl}${image}`} />}
     </Helmet>
   )
 }
