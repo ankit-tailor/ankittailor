930ca65840871159bf1c9ad7810213ec14658f54
ankit-tailor <tailorankit3345@gmail.com>
2022-10-16 01:07:58 +0530

    feat: link dark light

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,8 @@ module.exports = {
       dark: "#111827",
       white: "#f9fafb",
       active: "#929090",
+      linkDark: "#1f2937",
+      linkLight: "#e5e7eb",
       indigo: "rgba(0, 195, 255)",
       cyan: colors.cyan,
       blue: colors.blue,
