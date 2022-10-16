835f480dd3ab7c4c8fc56fa876dba0f5391ff5a7
ankit-tailor <tailorankit3345@gmail.com>
2022-10-17 02:13:01 +0530

    fix: import

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,5 @@
 import React from "react"
 import loadable from "@loadable/component"
-import React from "react"
 const About = loadable(() => import("../components/about/About"))
 const Layout = loadable(() => import("../components/layout/Layout"))
 const SEO = loadable(() => import("../components/SEO/seo"))
